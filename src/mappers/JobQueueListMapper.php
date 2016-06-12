e09ff2817ee15016fca62382c30119caa7e6f66a
Mathijs Corten <Mathijs Corten>
2016-06-12 16:17:37 +0200

    unused import

--- src/mappers/JobQueueListMapper.php
+++ src/mappers/JobQueueListMapper.php
@@ -4,7 +4,6 @@ namespace mcorten87\rabbitmq_api\mappers;
 
 
 use mcorten87\rabbitmq_api\jobs\JobBase;
-use mcorten87\rabbitmq_api\jobs\JobQueueList;
 use mcorten87\rabbitmq_api\objects\Method;
 use mcorten87\rabbitmq_api\objects\Url;
 use mcorten87\rabbitmq_api\services\MqManagementConfig;
