edc80c8fc32c8770c6cac51d9c02503bfcc72cdb
Mathijs Corten <info@mcorten.nl>
2017-01-24 09:18:13 +0100

    Fixes for unknown routing key returning null on __toString

--- src/objects/RoutingKeyUnknown.php
+++ src/objects/RoutingKeyUnknown.php
@@ -9,11 +9,16 @@
 namespace mcorten87\rabbitmq_api\objects;
 
 
-class RoutingKeyUnknown extends BaseObject
+class RoutingKeyUnknown extends RoutingKey
 {
     public function __construct()
     {
-        parent::__construct(null);
+        parent::__construct('');
+    }
+
+    public function getValue(): string
+    {
+        return '';
     }
 
     public function __toString(): string
